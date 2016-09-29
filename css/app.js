afd8b0a0b8169d8cb98022e68c331c2196f20362
Fausto Brito <G30@Faustos-MacBook-Air.local>
2016-09-29 18:29:13 -0400

    update on app and index

--- css/app.js
+++ css/app.js
@@ -1,40 +1,50 @@
 $(document).ready(function(){
     
- 	   $('select').material_select();
- 	   $('.carousel').carousel();
- 	//carousel nexts and previous movement methods.
+     $('select').material_select();
+     $('.carousel').carousel();
+  //carousel nexts and previous movement methods.
      /*$('.carousel.carousel-slider').carousel({full_width: true});*/
 
     // Next slide
-	$('.carousel').carousel('next');
-	$('.carousel').carousel('next', 3); // Move next n times.
-	// Previous slide
-	$('.carousel').carousel('prev');
-	$('.carousel').carousel('prev', 4); // Move prev n times.
-	// Set to nth slide
-	$('.carousel').carousel('set', 4);
-	//firebase data
-	
-  };
+  $('.carousel').carousel('next');
+  $('.carousel').carousel('next', 3); // Move next n times.
+  // Previous slide
+  $('.carousel').carousel('prev');
+  $('.carousel').carousel('prev', 4); // Move prev n times.
+  // Set to nth slide
+  $('.carousel').carousel('set', 4);
+  //firebase data
    var config = {
     apiKey: "AIzaSyBTKzJTmrtTbwJeS_V4sryVrYfHS1gIpP4",
     authDomain: "beer-d9b34.firebaseapp.com",
     databaseURL: "https://beer-d9b34.firebaseio.com",
     storageBucket: "",
     messagingSenderId: "773517315986"
+  };
   // initialize firebase
   firebase.initializeApp(config);
   
-   var database = firebase.database();
+
+
+  var database = firebase.database();
+  console.log("database " + database)
  var where = "";
  var cityState = "";
+
+ $(".cityStateInput").on("click", function(){
+  cityState = (this.value)
+  console.log("cityState " + cityState)
+  $(".input-field").html('<nav><div class="nav-wrapper"><form><div class="input-field bycity"><input id="whereInput" type="search" required class="yellow"><label for="search"><i class="material-icons">search</i></label><i class="material-icons">close</i></div><button type="submit" class="btn blue" id="clickButton">Submit</button><button type="submit" class="btn blue" id="searchAgainButton">Search Again?</button></form></div></nav>')
+ 
+
+
 
  // what happens when you click on the submit button
   $("#clickButton").on("click", function(){
    where = $("#whereInput").val().trim();
-   cityState = $(".cityStateInput").val().trim();
-   var queryURL = "https://crossorigin.me/http://api.brewerydb.com/v2/locations/?key=df96c11767682fe9178fde3cedaa19f3&" + cityState + "=" + where
-   console.log("where " + where)
+   var queryURL = "https://crossorigin.me/http://api.brewerydb.com/v2/locations/?key=9bebc0cee0d006506782667a99991706&" + cityState + "=" + where
+   // console.log("where " + where)
+   // console.log("cityState " + cityState)
   database.ref().push({
       where: where,
       cityState: cityState,
@@ -43,62 +53,52 @@ $(document).ready(function(){
     });
   return false;
   });
-// this need to return a brewery not a beer.... there doesn't seem to be a way to link a particular beer to a brewery....annoyingly
-  //  $("#clickclickButton").on("click", function(){
-  //  which = $("#whichInput").val().trim();
-  // var queryURL = "https://crossorigin.me/http://api.brewerydb.com/v2/beers/?key=df96c11767682fe9178fde3cedaa19f3&name=" + which
-  //  console.log(queryURL)
-  //  $.ajax({url: queryURL, method: 'GET'}).done(function(response){
-  //   var id = response.data[0].id;
-  //   console.log(id)
-  //   var queryURL = "https://crossorigin.me/http://api.brewerydb.com/v2/breweries/?key=df96c11767682fe9178fde3cedaa19f3&" + id
-  //  });
-  // database.ref().push({
-  //     which: which,
-  //     queryURL: queryURL,
-  //     dateAdded: firebase.database.ServerValue.TIMESTAMP
-  //   });
-  // return false;
-  // });
 
+  $("#searchAgainButton").on("click", function(){
+    $(".input-field").html('<button type="button" class="cityStateInput" value = "locality">By City</button><button type="button" class="cityStateInput" value = "region">ByState</button>')
+  })
 
+});
  database.ref().on("child_added", function(childSnapshot) {
-  console.log(childSnapshot.val().queryURL);
+  // console.log(childSnapshot.val().queryURL);
  
-  var queryURL = (childSnapshot.val().queryURL);
+  var queryURL =    (childSnapshot.val().queryURL);
       var map;
       // Create a new blank array for all the listing markers.
       var markers = [];
-      console.log("ssss " + where)
-      console.log(queryURL)
+      // console.log("ssss " + where)
+      // console.log(queryURL)
  $.ajax({url: queryURL, method: 'GET'}).done(function(response) {
        
-      console.log(response)
+      // console.log(response)
       
-       
-     
-        // Create a styles array to use with the map.
         
         // Constructor creates a new map - only center and zoom are required.
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 40.7413549, lng: -73.9980244},
-          zoom: 13,
+          zoom: 8,
           mapTypeControl: false
         });
         // These are the real estate listings that will be shown to the user.
         // Normally we'd have these in a database instead.
+        
         var lats = [];
         var lngs = [];
-        console.log(response.data.length)
+        console.log("length " + response.data.length)
         for (x=0;x<response.data.length;x++){
         var lat = response.data[x].latitude
         var lng = response.data[x].longitude
         var locations = [];
+        
         lats.push(lat);
         lngs.push(lng);
+        
       }
-      console.log(lats)
-      console.log(lngs)
+      // console.log(lats)
+      // console.log(lngs)
+
+     
+
         var largeInfowindow = new google.maps.InfoWindow();
         // Style the markers a bit. This will be our listing marker icon.
         var defaultIcon = ("https://raw.githubusercontent.com/bernerd252/secretbeer/master/images/beerBottle.gif")
@@ -110,19 +110,24 @@ $(document).ready(function(){
         for (var i = 0; i < lats.length; i++) {
           // Get the position from the location array. And turn them into something the computer can use.
           var latParse = parseFloat(lats[i])
-          console.log("latParse " + latParse)
+          // console.log("latParse " + latParse)
           var lngParse = parseFloat(lngs[i])
-          console.log("lngParse " + lngParse)
+          // console.log("lngParse " + lngParse)
           var title = response.data[i].brewery.name;
-          console.log(location)
+          // console.log(location)
           var description = response.data[i].brewery.description;
-          console.log("description" + description)
+          // console.log("description" + description)
           var streetAddress = response.data[i].streetAddress
-          console.log("street " + streetAddress)
+          // console.log("street " + streetAddress)
           var locality = response.data[i].locality
           var website = response.data[i].brewery.website;
-          var label = response.data[i].brewery.images.icon;
-          console.log(label)
+         var label;
+          if (response.data[i].brewery.images.icon == undefined) {
+            label = "http://www.truebeer.com/assets/images/European-Beer-Mug-1.jpg"
+          }
+           else {
+            var label = response.data[i].brewery.images.icon;}
+          // console.log(label)
       
       // change latParse and lngParse into latitude and longitude cocordinates that google maps can understand
          var position = new google.maps.LatLng(latParse,lngParse);
@@ -191,26 +196,33 @@ $(document).ready(function(){
       }
     }
   });
+// database.ref().on("child_added", function(childSnapshot) {
+//   // console.log(childSnapshot.val().queryURL);
+ 
+//   var queryURL =    (childSnapshot.val().queryURL);
+  
+//  $.ajax({url: queryURL, method: 'GET'}).done(function(response) {
+//  $('.carousel').empty();
+//  var logos = [];
+//  for (var j=0;j<response.data.length;j++){
+//   var logo = response.data[j].brewery.images.squareMedium;
+//   // console.log(response.data.length)
+//   // console.log(logo)
+//   logos.push(logo);
+//  }
+//  // console.log(logos)
+//     // Loops through the array of logos
+//     for (var i = 0; i < logos.length; i++){
+//       // console.log("boom")
+//       // Then dynamicaly generates buttons for each movie in the array
+
+//       // Note the jQUery syntax here... 
+//         var a = $('<a>') // This code $('<button>') is all jQuery needs to create the beginning and end tag. (<button></button>)
+//         a.addClass('carousel-item'); // Added a class 
+//         a.attr('data-name', logos[i]); // Added a data-attribute
+//         a.html('<img class="img" src="' + logos[i] + '">') // Provided the html
+//         $('.carousel').append(a); // Added the button to the HTML
+//         console.log("a " + a)
+    
+  });
 });
-      // This function takes in a COLOR, and then creates a new marker
-      // icon of that color. The icon will be 21 px wide by 34 high, have an origin
-      // of 0, 0 and be anchored at 10, 34).
-      // function makeMarkerIcon(markerColor) {
-      //   var markerImage = new google.maps.MarkerImage(
-      //     'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
-      //     '|40|_|%E2%80%A2',
-      //     new google.maps.Size(21, 34),
-      //     new google.maps.Point(0, 0),
-      //     new google.maps.Point(10, 34),
-      //     new google.maps.Size(21,34));
-      //   return markerImage;
-      // }
-  //  
-
-//
-	
-
-
-
-        
-        });
